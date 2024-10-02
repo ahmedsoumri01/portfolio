01f01f2cdd32d9903c0ab367c4ee417ece0c1c4e
ahmed soumri <ahmedsoumri01@gmail.com>
2024-10-02 16:59:26 +0100

    implement new methods and routes to the index

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db'); 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Admin = require('./models/Admin'); // Import Admin model
 
 // Load environment variables
 dotenv.config();
@@ -13,14 +16,42 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
- 
 
- 
- 
+const createDefaultAdmin = async () => {
+  try {
+    // Check if there is already an admin in the database
+    const existingAdmin = await Admin.findOne({ email: process.env.ADMIN_EMAIL });
+    
+    if (!existingAdmin) {
+      // If no admin exists, create a default one
+      const hashedPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, 10);
+      
+      const newAdmin = new Admin({
+        username: 'admin',
+        email: process.env.ADMIN_EMAIL,
+        password: hashedPassword,
+      });
+      
+      await newAdmin.save();
+      console.log("Default admin created successfully!");
+    } else {
+      console.log("Admin already exists. No need to create a default admin.");
+    }
+  } catch (error) {
+    console.error("Error creating default admin:", error);
+  }
+};
+
+// Create the default admin when the server starts
+createDefaultAdmin();
 
 // Set the port dynamically
 const PORT = process.env.PORT || 3001;
-
+//routes 
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/experiences', require('./routes/experienceRoutes'));
+/* app.use('/api/projects', require('./routes/projectRoutes')); */
+/* app.use('/api/skills', require('./routes/skillRoutes')); */
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 const server = app.listen(PORT, () => {
