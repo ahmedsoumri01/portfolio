854cb7d43558d6b9eadc4bfb4d90f7d50fe53e0e
ahmed soumri <ahmedsoumri01@gmail.com>
2024-10-02 15:19:45 +0100

    implement versel in backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db'); 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Load environment variables
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Connect to the database
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+ 
+
+ 
+ 
+
+// Set the port dynamically
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
