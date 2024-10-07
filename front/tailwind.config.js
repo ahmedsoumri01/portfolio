3f7e5e92d97bf1bcb2ead6dd4f269169b1a46b3c
ahmed soumri <ahmedsoumri01@gmail.com>
2024-10-07 09:54:22 +0100

    install packages , update tailwind config

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,9 +1,8 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class', // Add this line to enable dark mode via a class
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"    , "./node_modules/flowbite/**/*.js",    flowbite.content(),],
   theme: {
     extend: {},
   },
